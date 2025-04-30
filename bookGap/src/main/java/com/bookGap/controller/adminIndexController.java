95109dfddce93f4f193094c3eb227c20faf2ee50
kimsanghw <github tkdghk14@naver.com>
2025-04-30 15:52:05 +0900

    /admin/adminIndex.do /admin/adminBook.do adminController

--- bookGap/src/main/java/com/bookGap/controller/adminIndexController.java
+++ bookGap/src/main/java/com/bookGap/controller/adminIndexController.java
@@ -1,8 +1,21 @@
 package com.bookGap.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class adminIndexController {
-
+	
+	@RequestMapping(value = "admin/adminIndex.do", method = RequestMethod.GET)
+	public String adminIndex() {
+	
+		return "admin/adminIndex";
+	}
+	
+	@RequestMapping(value = "admin/adminBook.do", method = RequestMethod.GET)
+	public String adminBook() {
+	
+		return "admin/adminBook";
+	}
 }
