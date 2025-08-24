7cd4d5e881d8c146b9292e98141fa37e44c66aab
kimsanghw <github tkdghk14@naver.com>
2025-08-24 23:04:44 +0900

    어드민 환불관리 페이지

---
+++ bookGap/src/main/java/com/bookGap/dao/AdminRefundDAO.java
@@ -0,0 +1,8 @@
+package com.bookGap.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AdminRefundDAO {
+
+}
---
+++ bookGap/src/main/java/com/bookGap/service/AdminRefundService.java
@@ -0,0 +1,5 @@
+package com.bookGap.service;
+
+public interface AdminRefundService {
+
+}
